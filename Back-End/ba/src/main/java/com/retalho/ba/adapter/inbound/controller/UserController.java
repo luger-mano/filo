77f20ef25fe84e8cc28f999a06c80aec0529adea
luger-mano <lucas.germanysantos@gmail.com>
2025-07-24 23:58:42 -0300

    [feat] : I added comments for easy visualization of the methods' responsibility

--- Back-End/ba/src/main/java/com/retalho/ba/adapter/inbound/controller/UserController.java
+++ Back-End/ba/src/main/java/com/retalho/ba/adapter/inbound/controller/UserController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Controlador responsável por gerenciar as operações relacionadas aos usuários.
+ */
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
@@ -25,22 +28,44 @@ public class UserController {
     private final UpdateUserUseCasePort updateUserUseCasePort;
     private final DeleteUserUseCasePort deleteUserUseCasePort;
 
+    /**
+     * Cria um novo usuário
+     *
+     * @param userRequestDTO Objeto UserRequestDTO contendo os dados do novo usuário.
+     * @return O objeto UserResponseDTO criado, com os dados filtrados do usuário (sem expor dados).
+     */
     @PostMapping
     public ResponseEntity<UserResponseDTO>saveUser(@RequestBody @Valid UserRequestDTO userRequestDTO){
         UserResponseDTO user = saveUserUseCasePort.execute(userRequestDTO);
         return ResponseEntity.ok(user);
     }
+    /**
+     * Cria um novo usuário
+     *
+     * @return O objeto UserResponseDTO criado, com ID preenchido.
+     */
     @GetMapping
     public ResponseEntity<List<UserResponseDTO>>getUsers() throws UsersNotFoundException {
         return ResponseEntity.ok().body(getAllUserUseCasePort.execute());
     }
-
+    /**
+     * Cria um novo usuário
+     *
+     * @param userRequestDTO Objeto UserRequestDTO contendo os dados do novo usuário.
+     * @param id Objeto UUID é passado o id para atualizar determinado usuário.
+     * @return O objeto UserResponseDTO criado, com os dados filtrados do usuário (sem expor dados).
+     */
     @PutMapping("/update-user/{id}")
-    public ResponseEntity<UserResponseDTO>updateUser(@RequestBody UserRequestDTO userRequestDTO,
+    public ResponseEntity<UserResponseDTO>updateUser(@RequestBody @Valid UserRequestDTO userRequestDTO,
                                                      @PathVariable UUID id){
         return ResponseEntity.status(201).body(updateUserUseCasePort.execute(userRequestDTO, id));
     }
-
+    /**
+     * Cria um novo usuário
+     *
+     * @param id Objeto UUID é passado o id para deletar determinado usuário.
+     * @return O objeto String com uma mensagem de confirmação da deleção.
+     */
     @DeleteMapping("/delete-user/{id}")
     public ResponseEntity<String>deleteUser(@PathVariable UUID id){
         return ResponseEntity.status(201).body(deleteUserUseCasePort.execute(id));
