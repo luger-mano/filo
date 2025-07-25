1aefa75d4393f2cbcb28f37fa98c9290895c2c72
luger-mano <lucas.germanysantos@gmail.com>
2025-07-24 23:59:24 -0300

    [feat] : I put the field validations

--- Back-End/ba/src/main/java/com/retalho/ba/adapter/inbound/controller/request/UserRequestDTO.java
+++ Back-End/ba/src/main/java/com/retalho/ba/adapter/inbound/controller/request/UserRequestDTO.java
@@ -1,6 +1,9 @@
 package com.retalho.ba.adapter.inbound.controller.request;
 
 import com.retalho.ba.domain.enums.UserType;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -10,12 +13,19 @@ import java.util.UUID;
 public class UserRequestDTO {
 
     private UUID userId;
+    @NotBlank(message = "O campo name não pode estar em branco.")
     private String name;
+    @NotBlank(message = "O campo middleName não pode estar em branco.")
     private String middleName;
+    @NotBlank(message = "O campo email não pode estar em branco.")
     private String email;
+    @NotBlank(message = "O campo password não pode estar em branco.")
     private String password;
+    @NotBlank(message = "O campo cpf não pode estar em branco.")
     private String cpf;
+    @NotBlank(message = "O campo phone não pode estar em branco.")
     private String phone;
+    @NotNull(message = "O campo birth não pode ser nulo.")
     private LocalDate birth;
 
     public UserRequestDTO(UUID userId,
